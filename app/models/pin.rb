20bd37f8b717c210d45c7512b6110d820bb4d33c
ruby <maxmonterreyjr@yahoo.com>
2015-03-01 21:37:15 -0500

    validation on images and description as well as user name

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,8 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, :styles => {:large => "1100x1100>", :medium => "600x600>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
