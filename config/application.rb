fcbaf5d378c4b2ec09fe4d925cedcade7c4fb55b
Max Monterrey <maxmonterreyjr@yahoo.com>
2015-06-23 17:14:48 -0500

    New colors and fonts

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Pinteresting
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
+
+    #This setting are enable so that custom fonts can be used.
+        config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
   end
 end
